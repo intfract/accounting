5210a80686ad356c78c6361cbee13ffd9de0a134
Int Fract <110207136+intfract@users.noreply.github.com>
2022-11-03 03:47:06 +0000

    added debit and credit functions

--- main.dart
+++ main.dart
@@ -1,13 +1,35 @@
+// Accounts
+
 class Account {
   bool fluid = true;
+  bool dr = true;
   double balance = 0.0;
   Account([double balance = 0.0]) {
     this.balance = balance;
+  }
+
+  double debit(double amount) {
+    if (dr) {
+      this.balance += amount;
+    } else {
+      this.balance -= amount;
+    }
+    return this.balance;
+  }
+
+  double credit(double amount) {
+    if (!dr) {
+      this.balance += amount;
+    } else {
+      this.balance -= amount;
+    }
+    return this.balance;
   }
 }
 
 class Asset extends Account {
   bool fluid = false;
+  bool dr = true;
   double balance = 0.0;
   Asset([double balance = 0.0, bool fluid = false]) {
     this.balance = balance;
@@ -17,6 +39,7 @@ class Asset extends Account {
 
 class Liability extends Account {
   bool fluid = false;
+  bool dr = false;
   double balance = 0.0;
   Liability([double balance = 0.0, bool fluid = false]) {
     this.balance = balance;
@@ -25,6 +48,7 @@ class Liability extends Account {
 
 class Expense extends Account {
   bool fluid = false;
+  bool dr = true;
   double balance = 0.0;
   Expense([double balance = 0.0, bool fluid = false]) {
     this.balance = balance;
@@ -33,9 +57,25 @@ class Expense extends Account {
 
 class Income extends Account {
   bool fluid = false;
+  bool dr = false;
   double balance = 0.0;
   Income([double balance = 0.0, bool fluid = false]) {
     this.balance = balance;
+  }
+}
+
+// Books and Journals
+
+class CashBook {
+  String title = '';
+  List entries = [];
+  CashBook(String title, Account bank, Asset cash) {
+    this.title = title;
+    entries.add({
+      'date': DateTime.now(),
+      'bank': 0,
+      'cash': 0,
+    });
   }
 }
 
@@ -43,7 +83,15 @@ void main() {
   Account bank = new Asset(0, true);
   print(bank is Account);
   print(bank.fluid);
+  
   Asset cash = new Asset(100);
   print(cash is Account);
   print(cash.fluid);
+
+  Liability loan = new Liability(100);
+  loan.debit(50);
+  print(loan.balance);
+  
+  CashBook cashbook = new CashBook('cashbook', bank, cash);
+  print(cashbook.entries);
 }
